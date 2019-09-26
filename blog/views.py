3d17e96541d94495823a1cd36add43afb98abb89
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-09-26 20:25:03 +0200

    added base html template

--- blog/views.py
+++ blog/views.py
@@ -21,8 +21,14 @@ def home(request):
     context = {
         'data': posts
     }
-    return render(request, 'blog/home.html', context)
+    return render(request,
+                  'blog/home.html',
+                  context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request,
+                  'blog/about.html',
+                  {
+                      'title': 'about'
+                  })
