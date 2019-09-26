14bfb0e54a39f6969eac182b95d98dbdccd5a1ed
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-09-26 20:11:09 +0200

    added html pages and their references; posted dummy posts on the homepage

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# inject fake post on the page
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'John Doe first post',
+        'content': '''Hello! It's my first post here!''',
+        'date': '11/11/12'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'second post',
+        'content': 'testtesttesttesttesttesttesttesttesttesttesttesttesttest',
+        'date': '12/12/12'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<p>blog home<p>')
+    context = {
+        'data': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<p>blog about<p>')
+    return render(request, 'blog/about.html')
