d01cc2374e37706406bc373bbb5ee47222a72d43
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-09-29 22:53:12 +0200

    added magic method in blog/model.py, refactor some variables (change 'date' to 'posts'), change timezone in settings, delete dummy data from blog/views.py, register blog in admin site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # import user model
+    # when delete user, posts also will delete
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # magic method, return object title in query set instead of id
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-# inject fake post on the page
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'John Doe first post',
-        'content': '''Hello! It's my first post here!''',
-        'date': '11/11/12'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'second post',
-        'content': 'testtesttesttesttesttesttesttesttesttesttesttesttesttest',
-        'date': '12/12/12'
-    },
-]
+from .models import Post
 
 
 def home(request):
+
     context = {
-        'data': posts
+        'posts': Post.objects.all()
     }
     return render(request,
                   'blog/home.html',
