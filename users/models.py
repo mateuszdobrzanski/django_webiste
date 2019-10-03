f7355209ae3050036b9d6fcadcb4b04a47cd1b6f
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-10-03 22:20:04 +0200

    user can change profile pic and other data, profile pics are resized by pillow lib.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # one user has one profile and one profile is associated with one user
@@ -12,3 +13,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} profile'
+
+    # override and resize images over 300px
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -25,6 +25,34 @@ def register(request):
 
 # using this decorator for: user must be logged to see this view
 @login_required
-def profile (request):
+def profile(request):
     return render(request,
                   'users/profile.html')
+
+# using this decorator for: user must be logged to see this view
+@login_required
+def profile_update(request):
+    if request.method == 'POST':
+        # in brackets current information populated to the form
+        user_form = UserUpdateForm(request.POST,
+                                   instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                         request.FILES,
+                                         instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account bas been updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request,
+                  'users/profile_update.html',
+                  context)
